83ed674af6cfde3b3560f1fc49f98cfa679218d0
fokrule <fokrule.hasan@gmail.com>
2021-10-10 01:32:49 +0600

    Worked with ref, state, useEffect

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,26 @@
+import { useHistory } from "react-router";
+import NewMeetupForm from "../components/Meetups/NewMeetupForm"
+
 function NewMeetupPage() {
-    return <div>
-        New 
-    </div>
+    const history = useHistory();
+    function addMeetupHandker(meetupData) {
+        fetch(
+            'https://react-course-29dea-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            ).then(() => {
+                history.replace('/');
+            });
+    }
+    return <section>
+        <h1>New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandker}/>
+    </section>
 }
 
 export default NewMeetupPage
